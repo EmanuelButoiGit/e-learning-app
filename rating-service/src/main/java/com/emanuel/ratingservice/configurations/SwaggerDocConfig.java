a0b7ec7e411e40cb4a8f76132c985184ff809806
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-03-21 12:01:27 +0200

    Update version

--- rating-service/src/main/java/com/emanuel/ratingservice/configurations/SwaggerDocConfig.java
+++ rating-service/src/main/java/com/emanuel/ratingservice/configurations/SwaggerDocConfig.java
@@ -14,7 +14,7 @@ public class SwaggerDocConfig {
         return new OpenAPI().info(
                 new Info()
                         .title("Documentation")
-                        .version("1.0")
+                        .version("1.0.2")
                         .description("This service is responsible with all the operations related to ratings")
         );
     }
