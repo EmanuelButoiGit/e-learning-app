23ef78e1d1d394d7ad470ea117ef7da6a5212406
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-03-22 11:10:11 +0200

    Update rating.sql

--- DBScripts/rating.sql
+++ DBScripts/rating.sql
@@ -1,6 +1,8 @@
 CREATE TABLE rating (
     id SERIAL PRIMARY KEY,
     media_id BIGINT,
+    title VARCHAR(255),
+    description TEXT,
     general_rating REAL,
     tutor_rating REAL,
     content_rating REAL,
