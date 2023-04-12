30f7442aa033f469acea91eaf11716e71f9d5710
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-04-12 17:10:04 +0300

    Update RandomRecommendedMedia logic to retrieve random media even if general rating is lower than the minimum general rating

--- recommendation-service/src/main/java/com/emanuel/recommendationservice/configurations/SwaggerDocConfig.java
+++ recommendation-service/src/main/java/com/emanuel/recommendationservice/configurations/SwaggerDocConfig.java
@@ -14,7 +14,7 @@ public class SwaggerDocConfig {
         return new OpenAPI().info(
                 new Info()
                         .title("Documentation")
-                        .version("1.5")
+                        .version("1.6")
                         .description("This service is responsible with the recommendation workflow")
         );
     }
