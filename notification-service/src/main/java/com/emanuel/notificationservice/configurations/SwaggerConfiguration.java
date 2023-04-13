4c31e8df1ed053e282507ae32007fc33997ca1a9
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-04-13 22:54:50 +0300

    Create notification service
    Add a new microservice that sends mails to the admin.
    Created a scheduler but also an endpoint to send metrics.
    
    TO DO:
    Send daily mail with top medias (recommended)
    Send daily with metrics
    Send alerts when reaching a certain value
    Send mail when uploading a file
    Format mail add a picture
    see the meaning of each metrics

---
+++ notification-service/src/main/java/com/emanuel/notificationservice/configurations/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.emanuel.notificationservice.configurations;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(
+                new Info()
+                        .title("Documentation")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Butoi Emanuel-Sebastian")
+                                .url("https://www.linkedin.com/in/emanuel-sebastian-butoi-929271213/"))
+                        .description("This service is responsible with sending the admin notifications")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                        .termsOfService("https://docs.github.com/en/github/site-policy/github-terms-of-service"))
+                        .externalDocs(new ExternalDocumentation()
+                                .description("Learn more about my API")
+                                .url("https://github.com/EmanuelButoiGit?tab=repositories")
+        );
+    }
+}
