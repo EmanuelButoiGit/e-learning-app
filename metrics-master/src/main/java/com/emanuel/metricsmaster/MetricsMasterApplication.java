48b34f60aba94ded6b9dbd9bb9fbdad261c45a8d
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-04-13 18:31:48 +0300

    Add observability
    Create metrics-master project and setup Actuator, Prometheus, and Grafana which are all valuable tools for achieving observability.
    
    TO DO: Add this in the API Gateway

---
+++ metrics-master/src/main/java/com/emanuel/metricsmaster/MetricsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel.metricsmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsMasterApplication.class, args);
+	}
+
+}
---
+++ metrics-master/src/test/java/com/emanuel/metricsmaster/MetricsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.metricsmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
