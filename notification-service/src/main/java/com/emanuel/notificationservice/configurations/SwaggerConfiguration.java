89a4f598b65ea450937d4a780f73a67a1f6f20d6
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-04-15 14:23:43 +0300

    Update documentation version

--- notification-service/src/main/java/com/emanuel/notificationservice/configurations/SwaggerConfiguration.java
+++ notification-service/src/main/java/com/emanuel/notificationservice/configurations/SwaggerConfiguration.java
@@ -17,7 +17,7 @@ public class SwaggerConfiguration {
         return new OpenAPI().info(
                 new Info()
                         .title("Documentation")
-                        .version("1.3")
+                        .version("1.4")
                         .contact(new Contact()
                                 .name("Butoi Emanuel-Sebastian")
                                 .url("https://www.linkedin.com/in/emanuel-sebastian-butoi-929271213/"))
