afa8917cf90a1ed91dc3ddebf930b37b117eeb6d
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-02 19:39:14 +0300

    Update SanitizationService.java

--- starter-library/src/main/java/com/emanuel/starterlibrary/services/SanitizationService.java
+++ starter-library/src/main/java/com/emanuel/starterlibrary/services/SanitizationService.java
@@ -9,16 +9,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+
 @Service
 public class SanitizationService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SanitizationService.class);
     @SneakyThrows
-    public String sanitizeString(String string){
+    public String sanitizeString(@Valid String input){
         try {
             Safelist safelist = Safelist.basic();
             Cleaner cleaner = new Cleaner(safelist);
-            return cleaner.clean(Jsoup.parse(string)).text();
+            return cleaner.clean(Jsoup.parse(input)).text();
         } catch (Exception e){
             String errorMessage = "Could not sanitize the input. " + e.getMessage();
             LOGGER.error(e.getMessage());
