bf46c4f7537c2f677bd74bfa7b89632de5a2064b
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-05 19:30:39 +0300

    Add echo messages to bats

--- 1-Tools/start-discovery-server.bat
+++ 1-Tools/start-discovery-server.bat
@@ -1,3 +1,4 @@
 @echo off
+echo Starting "discovery-server.jar"...
 java -jar C:\Users\Emanuel\Documents\GitHub\e-learning-app\discovery-server\jar\discovery-server.jar
 pause
--- 1-Tools/start-sonar-server.bat
+++ 1-Tools/start-sonar-server.bat
@@ -2,5 +2,4 @@
 echo Starting "start-sonar-server.bat"...
 cd /d D:\Tools\sonarqube-10.0.0.68432\bin\windows-x86-64\
 call StartSonar.bat
-echo SonarQube has been started.
 pause
