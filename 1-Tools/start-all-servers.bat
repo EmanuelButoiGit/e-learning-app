48f8d9c6be30e01cf0436560bfc35debf6695602
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-05 19:46:41 +0300

    Create start-all-servers.bat

---
+++ 1-Tools/start-all-servers.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-discovery-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-zipkin-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-sonar-server.bat"
