8a11c05625b4b7825b1c6007eb60eeaebb25f631
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-05 19:48:58 +0300

    Create start-all-utils.bat

---
+++ 1-Tools/start-all-utils.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && app-properties.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && sonar-analyze.bat"
