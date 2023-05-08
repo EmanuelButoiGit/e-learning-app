ef33d1570aeca7fe1dd7e988336c00640057956d
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-08 22:57:36 +0300

    Remove "@SneakyThrows"

--- recommendation-service/src/main/java/com/emanuel/recommendationservice/proxies/MediaServiceProxy.java
+++ recommendation-service/src/main/java/com/emanuel/recommendationservice/proxies/MediaServiceProxy.java
@@ -3,7 +3,6 @@ package com.emanuel.recommendationservice.proxies;
 import com.emanuel.starterlibrary.dtos.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import lombok.SneakyThrows;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,35 +12,30 @@ import java.util.List;
 
 @FeignClient("media-service")
 public interface MediaServiceProxy {
-    @SneakyThrows
     @GetMapping("api/media")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get all media files")
     @ApiResponse(responseCode = "200", description = "All media files retrieved")
     List<MediaDto> getAllMedias();
 
-    @SneakyThrows
     @GetMapping("api/audio")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get all audio files")
     @ApiResponse(responseCode = "200", description = "All audio audio retrieved")
     List<AudioDto> getAllAudios();
 
-    @SneakyThrows
     @GetMapping("api/document")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get all document files")
     @ApiResponse(responseCode = "200", description = "All document files retrieved")
     List<DocumentDto> getAllDocuments();
 
-    @SneakyThrows
     @GetMapping("api/image")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get all image files")
     @ApiResponse(responseCode = "200", description = "All image files retrieved")
     List<ImageDto> getAllImages();
 
-    @SneakyThrows
     @GetMapping("api/video")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get all video files")
