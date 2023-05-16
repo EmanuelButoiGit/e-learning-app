8d56bedf3f2137a4a4a1ea01a27c38c1a187415d
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-16 17:52:35 +0300

    Create custom annotation to enable all the resilient/mitigation strategies

---
+++ starter-library/src/main/java/com/emanuel/starterlibrary/annotations/Resilient.java
@@ -0,0 +1,23 @@
+package com.emanuel.starterlibrary.annotations;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Retry(name = "default")
+@CircuitBreaker(name = "default")
+@RateLimiter(name = "default")
+@Bulkhead(name = "default")
+@TimeLimiter(name = "default")
+public @interface Resilient {
+
+}
