d3e7deb05dc0ed5ed33442b39b99a683f83d792b
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-22 22:28:20 +0300

    Open servers in the browsers

--- 1-Tools/start-prometheus-server.bat
+++ 1-Tools/start-prometheus-server.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Starting "prometheus.exe"...
+start "" http://localhost:9090/
 cd /d D:\Tools\prometheus-2.43.0
 .\prometheus.exe
 pause
--- 1-Tools/start-zipkin-server.bat
+++ 1-Tools/start-zipkin-server.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Starting "zipkin-server.jar"...
+start "" http://localhost:9411/zipkin/
 java -jar D:\Tools\zipkin-server.jar
 pause
