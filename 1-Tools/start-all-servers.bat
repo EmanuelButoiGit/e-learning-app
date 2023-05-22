d477f7f93c397f5e9b16b21bee9274d09d53e68d
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-22 22:13:07 +0300

    Update start-all-servers.bat

--- 1-Tools/start-all-servers.bat
+++ 1-Tools/start-all-servers.bat
@@ -6,3 +6,4 @@ start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools 
 start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-grafana-server.bat"
 start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-logstash-server.bat"
 start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-elastic-search-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-kibana-server.bat"
