79d624e728a93b94e9db9e3cc151c8b43e9888df
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-22 20:12:21 +0300

    Run the bats directly and remove start command
    FIX: the start command was adding a new cmd

--- 1-Tools/start-all.bat
+++ 1-Tools/start-all.bat
@@ -1,8 +1,5 @@
 @echo off
 rem Start all servers:
-start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-discovery-server.bat"
-start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-zipkin-server.bat"
-start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-sonar-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start-all-servers.bat"
 rem Start all the utility scripts:
-start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && app-properties.bat"
-start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && sonar-analyze.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start-all-utils.bat"
