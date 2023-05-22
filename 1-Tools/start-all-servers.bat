a3a790847c79e550bb78cbf30e5d68532dce8a69
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-22 21:54:42 +0300

    Update start-all-servers.bat

--- 1-Tools/start-all-servers.bat
+++ 1-Tools/start-all-servers.bat
@@ -4,3 +4,5 @@ start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools 
 start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-sonar-server.bat"
 start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-prometheus-server.bat"
 start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-grafana-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-logstash-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-elastic-search-server.bat"
