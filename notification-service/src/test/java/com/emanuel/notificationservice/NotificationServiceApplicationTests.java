4ea345d9f1c6eb40f8d5734d002138e6b8a0e6a4
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-23 21:18:47 +0300

    Update NotificationServiceApplicationTests.java

--- notification-service/src/test/java/com/emanuel/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/emanuel/notificationservice/NotificationServiceApplicationTests.java
@@ -1,5 +1,8 @@
 package com.emanuel.notificationservice;
 
+import com.emanuel.notificationservice.services.NotificationService;
+import com.emanuel.starterlibrary.StarterLibraryApplication;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,8 @@ class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		NotificationServiceApplication.main(new String[]{});
+		Assertions.assertTrue(true, "Application context loaded successfully.");
 	}
 
 }
