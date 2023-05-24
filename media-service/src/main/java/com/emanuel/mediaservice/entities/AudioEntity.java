d3de8ce3bcfcc4317f716458cb31e6de989d5a00
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-05-24 19:41:14 +0300

    Remove unused imports

--- media-service/src/main/java/com/emanuel/mediaservice/entities/AudioEntity.java
+++ media-service/src/main/java/com/emanuel/mediaservice/entities/AudioEntity.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Data
 @Entity
