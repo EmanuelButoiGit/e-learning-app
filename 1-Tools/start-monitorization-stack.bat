7e89c653794e85876e654aa3b5f938f478d90696
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-06-17 15:37:33 +0300

    Create start-monitorization-stack.bat

---
+++ 1-Tools/start-monitorization-stack.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-prometheus-server.bat"
+start "" cmd /c "cd /d C:\Users\Emanuel\Documents\GitHub\e-learning-app\1-Tools && start start-grafana-server.bat"
